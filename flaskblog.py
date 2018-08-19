8ec06c447e9dfb0a78c8ed4ef53dfd2d330209bb
Samuel Barksdale <sam@SamBarkalesMBP3.lan>
2018-08-18 17:06:29 -0700

    Set up Pycharm and rendered_template home page

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 # double underscore variable is the name of a module
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return "<h1>About Page!<h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
